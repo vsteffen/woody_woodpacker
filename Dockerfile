a3102f8979cff9dee02c89e5d0da7357fdb4d6bc
vsteffen <vsteffen@student.42.fr>
2020-02-23 18:13:17 +0100

    Update Dockerfile / run.sh now build image if it's not present

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ CMD ["/bin/bash"]
 RUN rm /etc/dpkg/dpkg.cfg.d/excludes
 
 RUN apt update \
-	&& apt install -y git vim man gcc clang make \
+	&& apt install -y git vim man gcc clang make nasm \
 	&& mkdir /root/woody_woodpacker
 
 WORKDIR /root/woody_woodpacker
--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker run -it --mount type=bind,source=$(pwd),target=/root/woody_woodpacker woody
+
+IMAGE_NAME="woody"
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [[ "$(docker images -q woody 2> /dev/null)" == "" ]]; then
+	docker build -t "$IMAGE_NAME" "$SCRIPT_PATH"
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
+fi
+
+docker run -it --mount type=bind,source="$SCRIPT_PATH",target=/root/woody_woodpacker woody
